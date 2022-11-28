2797cf63d80ae1831a5fdd1a5f88f12702eae32e
NayyarAbhi <95123720+NayyarAbhi@users.noreply.github.com>
2022-11-28 11:20:26 +0000

    Add files via upload

---
+++ azureSql.js
@@ -0,0 +1,54 @@
+// Import the mssql package
+var sql = require("mssql");
+
+// declaring variables
+var conn;
+let req;
+
+// Create a configuration object for our Azure SQL connection parameters
+var dbConfig = {
+ server: "servermortgages.database.windows.net",
+ database: "Mortgages", 
+ user: "test",
+ password: "Password@1",
+ port: 1433,
+ options: {
+       encrypt: true
+   }
+};
+
+function setConnection() {
+    conn = new sql.ConnectionPool(dbConfig);
+}
+
+function closeConnection() {
+    conn.close();
+}
+
+function setRequest() {
+    req = new sql.Request(conn);
+}
+
+function getRecord(query) {
+    conn.connect()
+    .then(function () {
+      req.query(query)
+      .then(function (recordset) {
+        closeConnection();
+        console.log(recordset);
+      })
+      .catch(function (err) {
+        console.log(err);
+        closeConnection();
+      })
+    })
+    .catch(function (err) {
+      console.log(err);
+      closeConnection();
+    });
+}
+
+var query = "SELECT TOP 2 * FROM [SalesLT].[Customer]";
+setConnection();
+setRequest();
+getRecord(query);
